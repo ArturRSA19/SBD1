67dc19b2ac6ab1a62f5269a55e6474e585e4c7c1
Artur Rodrigues <artur190503@outlook.com>
2023-06-19 22:25:36 -0300

    Add - Apaga.sql

---
+++ Exercícios das aulas/Aula10Extra1/aula10extra1Apaga_ArturAlves_211043638.sql
@@ -0,0 +1,20 @@
+﻿-- -------- < Aula 10 Exercício Extra 1 > --------
+--
+--                    SCRIPT DE APAGA (DDL)
+--
+-- Data Criacao ...........: 19/06/2023
+-- Autor(es) ..............: Artur Rodrigues Sousa Alves
+-- Banco de Dados .........: MySQL 8.0
+-- Base de Dados (nome) ...: aula10extra1
+--
+-- PROJETO => 2 DROP TABLE
+-- 
+-- Ultimas Alteracoes
+--   19/06/2023 => Criação do SCRIPT para apagar as tabelas da base de dados
+--
+-- ---------------------------------------------------------
+
+USE aula10extra1;
+
+DROP TABLE CIDADE;
+DROP TABLE ESTADO;
